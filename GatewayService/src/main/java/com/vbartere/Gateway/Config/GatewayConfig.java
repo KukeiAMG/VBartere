a70d2d58ea2af69b46b7d9289893002d7996bc08
MerrikM <merrikm.pl03@gmail.com>
2024-10-28 15:53:05 +0500

    getUserIdFromUserService method
    Добавлен метод для получения id текущего аутентифицированного пользователя из UserService. Обновлен AdvertisementRoute

--- GatewayService/src/main/java/com/vbartere/Gateway/Config/GatewayConfig.java
+++ GatewayService/src/main/java/com/vbartere/Gateway/Config/GatewayConfig.java
@@ -1,7 +1,5 @@
 package com.vbartere.Gateway.Config;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -20,9 +18,9 @@ public class GatewayConfig {
     }
 
     @Bean
-    public RouteLocator CustomRoute (RouteLocatorBuilder builder) {
+    public RouteLocator AdvertisementRoute (RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("AdvertisementService", r -> r.path("/api/advertisements/create")
+                .route("AdvertisementService", r -> r.path("/api/advertisements/{id}/update", "/api/advertisements/{id}/delete")
                         .filters(f -> f.filter((exchange, chain) -> {
                             // Проверка JWT и phoneNumber
                             // В Headers добавляем ключи Authorization и phoneNumber, а значения - токен и телефон юзера
@@ -31,8 +29,9 @@ public class GatewayConfig {
 
                             assert phoneHeader != null;
 
-                            if (authHeader != null && authHeader.startsWith("Bearer")) {
+                            if (authHeader != null && authHeader.startsWith("Bearer ")) {
                                 String jwtToken = authHeader.substring(7);
+                                System.out.println(jwtToken);
                                 if (validateJwt(jwtToken, phoneHeader)) {
                                     return chain.filter(exchange);
                                 }
@@ -41,12 +40,37 @@ public class GatewayConfig {
                             return exchange.getResponse().setComplete();
                         }))
                         .uri("http://localhost:8080")) // URL AdvertisementService
+
+                .route("AdvertisementService", r -> r.path("/api/advertisements/create")
+                        .filters(f -> f.filter((exchange, chain) -> {
+                            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+                            String phoneHeader = exchange.getRequest().getHeaders().getFirst("phoneNumber");
+
+                            assert phoneHeader != null;
+
+                            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                                String jwtToken = authHeader.substring(7);
+                                Long userId = getUserIdFromUserService(jwtToken);
+                                if (validateJwt(jwtToken, phoneHeader)) {
+                                    if (userId != null) {
+                                        exchange.getRequest().mutate()
+                                                .header("user-ID", userId.toString())
+                                                .build();
+                                        return chain.filter(exchange);
+                                    }
+                                }
+                            }
+                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return exchange.getResponse().setComplete();
+                        }))
+                        .uri("http://localhost:8080"))
+
                 .build();
     }
 
     private boolean validateJwt(String jwtToken, String phoneNumber) {
         try {
-            // Создание URL с параметрами запроса для валидации3
+            // Создание URL с параметрами запроса для валидации
             String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/users/validate")
                     .queryParam("token", jwtToken)
                     .queryParam("phoneNumber", phoneNumber)
@@ -54,8 +78,8 @@ public class GatewayConfig {
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(jwtToken);
 
-            // Создание HttpEntity без тела, если не нужно передавать дополнительные данные в теле запроса
             HttpEntity<String> entity = new HttpEntity<>(headers);
 
             ResponseEntity<Boolean> response = restTemplate.exchange(
@@ -71,17 +95,26 @@ public class GatewayConfig {
         }
     }
 
-    private static final String SECRET_KEY = "5FZsRG9Q2f9UvdxeUR4iU5FV9nFg1Hn9zPb49M8uV7o=";
-    private Long extractUserId(String token) {
+    private Long getUserIdFromUserService(String jwtToken) {
         try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(SECRET_KEY.getBytes())
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-            return Long.parseLong(claims.getSubject()); // Предполагается, что ID пользователя хранится в subject
+            String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/users/getCurrentUserId")
+                    .queryParam("token", jwtToken)
+                    .toUriString();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(jwtToken);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<Long> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    Long.class);
+
+            return response.getBody();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error retrieving user ID from UserService: " + e.getMessage());
             return null;
         }
     }
