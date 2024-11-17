12055c2fe65ce78331e3e4af8be42bbd8aa8e738
MerrikM <merrikm.pl03@gmail.com>
2024-11-17 14:33:25 +0500

    added AdvertisementDTO class to UserService

---
+++ userService/src/main/java/com/vbartere/userservice/DTO/AdvertisementDTO.java
@@ -0,0 +1,27 @@
+package com.vbartere.userservice.DTO;
+
+public class AdvertisementDTO {
+    private Long id;
+    private String title;
+
+    public AdvertisementDTO(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
