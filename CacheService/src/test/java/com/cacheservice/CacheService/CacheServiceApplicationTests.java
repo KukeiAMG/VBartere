da1eda31c0c95de054f6e006075e967b20b7d01b
MerrikM <merrikm.pl03@gmail.com>
2025-04-18 16:00:20 +0500

    Added new CacheService

---
+++ CacheService/src/test/java/com/cacheservice/CacheService/CacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacheservice.CacheService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
