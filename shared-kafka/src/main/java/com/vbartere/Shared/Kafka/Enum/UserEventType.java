7ab71d72040f5b7198e19ea9420db9a9e51b1451
MerrikM <merrikm.pl03@gmail.com>
2025-04-30 17:08:59 +0500

    refactoring

---
+++ shared-kafka/src/main/java/com/vbartere/Shared/Kafka/Enum/UserEventType.java
@@ -0,0 +1,6 @@
+package com.vbartere.Shared.Kafka.Enum;
+
+public enum UserEventType {
+    USER_CREATED,
+    USER_UPDATED,
+}
---
+++ shared-kafka/src/main/java/com/vbartere/Shared/Kafka/Events/CartResult.java
@@ -0,0 +1,50 @@
+package com.vbartere.Shared.Kafka.Events;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CartResult {
+    private Long userId;
+    private Long advertisementId;
+    private boolean successful;
+
+    @JsonCreator
+    public CartResult(@JsonProperty("userId") Long userId, @JsonProperty("advertisementId") Long advertisementId, @JsonProperty("successful") boolean successful) {
+        this.userId = userId;
+        this.advertisementId = advertisementId;
+        this.successful = successful;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getAdvertisementId() {
+        return advertisementId;
+    }
+
+    public void setAdvertisementId(Long advertisementId) {
+        this.advertisementId = advertisementId;
+    }
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        this.successful = successful;
+    }
+
+    @Override
+    public String toString() {
+        return "CartResult{" +
+                "userId=" + userId +
+                ", advertisementId=" + advertisementId +
+                ", successful=" + successful +
+                '}';
+    }
+}
