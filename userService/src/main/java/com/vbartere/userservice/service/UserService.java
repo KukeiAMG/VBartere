432ac325f71bf678f65868c3cbe28941e3322791
MerrikM <merrikm.pl03@gmail.com>
2025-05-04 18:25:03 +0500

    Update UserService.java

--- userService/src/main/java/com/vbartere/userservice/service/UserService.java
+++ userService/src/main/java/com/vbartere/userservice/service/UserService.java
@@ -1,12 +1,22 @@
 package com.vbartere.userservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vbartere.Shared.Kafka.Enum.UserEventType;
+import com.vbartere.Shared.Kafka.Events.UserEvent;
+import com.vbartere.userservice.model.Cart;
 import com.vbartere.userservice.model.Role;
 import com.vbartere.userservice.model.User;
+import com.vbartere.userservice.repository.CartRepository;
 import com.vbartere.userservice.repository.RoleRepository;
 import com.vbartere.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -15,36 +25,70 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final RoleRepository roleRepository;
+    private final CartRepository cartRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final PasswordEncoder passwordEncoder;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public UserService(UserRepository userRepository, JwtService jwtService, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, JwtService jwtService, RoleRepository roleRepository, CartRepository cartRepository, KafkaTemplate<String, String> kafkaTemplate, PasswordEncoder passwordEncoder, ObjectMapper objectMapper) {
         this.userRepository = userRepository;
         this.jwtService = jwtService;
         this.roleRepository = roleRepository;
+        this.cartRepository = cartRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.passwordEncoder = passwordEncoder;
+        this.objectMapper = objectMapper;
     }
 
-    public String loginUser(String phoneNumber, String password) {
+    @Transactional(readOnly = true)
+    public String loginUser(String phoneNumber, String password) throws JsonProcessingException {
         User user = userRepository.findByPhoneNumber(phoneNumber)
                 .filter(u -> u.getPassword().equals(password))
                 .orElseThrow(() -> new IllegalArgumentException("wrong phone number or password"));
+
+        UserEvent userEvent = new UserEvent(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                UserEventType.USER_LOGIN
+        );
+
+        kafkaTemplate.send("user-event", objectMapper.writeValueAsString(userEvent));
 
         // Генерация JWT токена
         return jwtService.generateToken(user.getPhoneNumber());
     }
 
-    public User registerUser(String phoneNumber, String password) {
+    @Transactional
+    public User registerUser(String phoneNumber, String password) throws JsonProcessingException {
         if (userRepository.findByPhoneNumber(phoneNumber).isPresent()) {
             throw new IllegalArgumentException("user already exists");
         }
         User user = new User();
         user.setPhoneNumber(phoneNumber);
         user.setPassword(passwordEncoder.encode(password));
-        return userRepository.save(user);
+
+        user = userRepository.save(user);
+
+        Cart cart = new Cart();
+        cart.setUserId(user.getId());
+        cart.setAdvertisementList(new ArrayList<>());
+        cartRepository.save(cart);
+
+        UserEvent userEvent = new UserEvent(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                UserEventType.USER_CREATED
+        );
+
+        kafkaTemplate.send("user-event", objectMapper.writeValueAsString(userEvent));
+
+        return user;
     }
 
+    @Transactional
     public User assignRoleToUser(Long userId, String roleName) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("user not found"));
@@ -62,15 +106,32 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public User updateUserDetails(Long userId, String name, String surname) {
+    @Transactional
+    public User updateUserDetails(Long userId, String name, String surname) throws JsonProcessingException {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("user not found"));
         user.setName(name);
         user.setSurname(surname);
+
+        UserEvent userEvent = new UserEvent(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                UserEventType.USER_UPDATED
+        );
+        kafkaTemplate.send("user-event", objectMapper.writeValueAsString(userEvent));
+
         return userRepository.save(user);
     }
 
+    @Transactional(readOnly = true)
+    public Long getUserIdByPhoneNumber(String token) {
+        User user = userRepository.findByPhoneNumber(jwtService.extractPhoneNumber(token))
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+        return user.getId();
+    }
 
+    @Transactional(readOnly = true)
     public boolean isPhoneNumberRegistered(String phoneNumber) {
         return userRepository.findByPhoneNumber(phoneNumber).isPresent();
     }
