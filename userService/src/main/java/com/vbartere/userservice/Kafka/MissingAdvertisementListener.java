37b5c2a16b2f7b07f902e05bf57edca6199a0960
MerrikM <merrikm.pl03@gmail.com>
2025-05-04 19:17:08 +0500

    Update MissingAdvertisementListener.java

--- userService/src/main/java/com/vbartere/userservice/Kafka/MissingAdvertisementListener.java
+++ userService/src/main/java/com/vbartere/userservice/Kafka/MissingAdvertisementListener.java
@@ -1,5 +1,7 @@
 package com.vbartere.userservice.Kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vbartere.Shared.Kafka.CartResult;
 import com.vbartere.userservice.service.CartService;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,14 +11,17 @@ import org.springframework.stereotype.Service;
 public class MissingAdvertisementListener {
 
     private final CartService cartService;
+    private final ObjectMapper objectMapper;
 
-    public MissingAdvertisementListener(CartService cartService) {
+    public MissingAdvertisementListener(CartService cartService, ObjectMapper objectMapper) {
         this.cartService = cartService;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(topics = "missing-advertisements", groupId = "advertisement-group",
             containerFactory = "kafkaListenerContainerFactory")
-    public void handleCartEvent(CartResult result) {
+    public void handleCartEvent(String message) throws JsonProcessingException {
+        CartResult result = objectMapper.readValue(message, CartResult.class);
         if (result.isSuccessful()) {
             System.out.println("Объявление с ID " + result.getAdvertisementId() + " успешно обработано.");
             // TODO notifyUser("Объявление успешно обработано.");
