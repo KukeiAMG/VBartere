60fd917fe0511b3eeaf7980bf5d7a1f77e1e920b
MerrikM <merrikm.pl03@gmail.com>
2025-05-04 18:24:46 +0500

    Create CartRepository.java

---
+++ userService/src/main/java/com/vbartere/userservice/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.vbartere.userservice.repository;
+
+import com.vbartere.userservice.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+}
