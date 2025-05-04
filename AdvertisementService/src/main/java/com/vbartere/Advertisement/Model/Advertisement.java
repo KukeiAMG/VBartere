01f6ae1479afbce59df092b02be06608688714dd
MerrikM <merrikm.pl03@gmail.com>
2025-05-04 19:28:07 +0500

    Update Advertisement.java

--- AdvertisementService/src/main/java/com/vbartere/Advertisement/Model/Advertisement.java
+++ AdvertisementService/src/main/java/com/vbartere/Advertisement/Model/Advertisement.java
@@ -12,8 +12,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "advertisement")
-@Getter
-@Setter
 public class Advertisement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,15 +33,18 @@ public class Advertisement {
     @JsonBackReference
     private SubCategory subcategory;
 
-    @OneToMany(mappedBy = "advertisement", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
+    @OneToMany(mappedBy = "advertisement", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE }, fetch = FetchType.EAGER)
     @JsonManagedReference
     private List<Image> imageList;
 
+    @Column(name = "ownerId")
     private Long ownerId; // id пользователя из UserService (owner)
 
+    @Column(name = "buyersId")
     private Long buyersId;
 
-    private boolean status;
+    @Column(name = "status")
+    private Boolean status;
 
     public Advertisement() {}
 
@@ -54,6 +55,70 @@ public class Advertisement {
         this.imageList = imageList;
         this.ownerId = ownerId;
         this.buyersId = buyersId;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotEmpty(message = "Title can not be empty") @Size(min = 2, max = 50, message = "The minimum allowed number of characters is 2, the maximum is 50") String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NotEmpty(message = "Title can not be empty") @Size(min = 2, max = 50, message = "The minimum allowed number of characters is 2, the maximum is 50") String title) {
+        this.title = title;
+    }
+
+    public @NotEmpty(message = "Description can not be empty") @Size(min = 2, max = 500, message = "The minimum allowed number of characters is 2, the maximum is 500") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NotEmpty(message = "Description can not be empty") @Size(min = 2, max = 500, message = "The minimum allowed number of characters is 2, the maximum is 500") String description) {
+        this.description = description;
+    }
+
+    public SubCategory getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(SubCategory subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<Image> imageList) {
+        this.imageList = imageList;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Long getBuyersId() {
+        return buyersId;
+    }
+
+    public void setBuyersId(Long buyersId) {
+        this.buyersId = buyersId;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
         this.status = status;
     }
 
