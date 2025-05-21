407129ac9e0dc753ba7a2de730806b22a25387a1
KukeiAMG <159035810+KukeiAMG@users.noreply.github.com>
2025-05-21 20:09:27 +0500

    init

---
+++ src/main/java/com/example/Vbartere/VbartereApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vbartere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbartereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbartereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Vbartere/VbartereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vbartere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbartereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
