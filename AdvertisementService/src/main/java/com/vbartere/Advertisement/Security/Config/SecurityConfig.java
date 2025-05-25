9ea196b1a24ada683c06d15529a4fafad6d1a32e
MerrikM <merrikm.pl03@gmail.com>
2025-05-25 17:45:23 +0500

    Update SecurityConfig.java

--- AdvertisementService/src/main/java/com/vbartere/Advertisement/Security/Config/SecurityConfig.java
+++ AdvertisementService/src/main/java/com/vbartere/Advertisement/Security/Config/SecurityConfig.java
@@ -2,15 +2,19 @@ package com.vbartere.Advertisement.Security.Config;
 
 
 import com.vbartere.Advertisement.Security.JwtFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -24,6 +28,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+    private static final String INTERNAL_SECRET = "payment-service-secret";
 
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -42,6 +47,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/api/advertisements/{id}/update-fields").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.PUT, "/api/advertisements/{id}/update-images").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.DELETE, "/images/{id}").hasAuthority("ROLE_USER")
+                        .requestMatchers(HttpMethod.POST, "/api/advertisements/owners")
+                        .access(internalHeaderAuthorizationManager())
 //                        .requestMatchers("/api/subcategory/create", "/api/subcategory/{id}/delete",
 //                                "/api/subcategory/{id}/update").hasAuthority("ROLE_ADMIN")
 //                        .requestMatchers("/api/category/create", "/api/category/{id}/update",
@@ -50,6 +57,15 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    public AuthorizationManager<RequestAuthorizationContext> internalHeaderAuthorizationManager() {
+        return (authenticationSupplier, context) -> {
+            HttpServletRequest request = context.getRequest();
+            String header = request.getHeader("Internal-Call");
+            boolean granted = INTERNAL_SECRET.equals(header);
+            return new AuthorizationDecision(granted);
+        };
     }
 
     @Bean
